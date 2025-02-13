d416c28861a5d357f7f7f3c0aa6fb961a931a05e
Milou Sanden <Milou.Sanden@gmail.com>
2025-02-13 12:08:28 +0100

    connecting api and currency exchange

---
+++ src/connect_api.py
@@ -0,0 +1,47 @@
+import os
+import json
+import logging
+from dotenv import load_dotenv
+import requests
+from requests import Session, RequestException
+
+load_dotenv()
+API_KEY = os.getenv("COINMARKET_API")
+EXCHANGE_KEY = os.getenv("EXCHANGE_API")
+
+API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+EXCHANGE_URL ="https://v6.exchangerate-api.com/v6/c9f25a95a4c73d272faed791/latest/USD"
+
+def get_exchange():
+    try: 
+        response = requests.get(EXCHANGE_KEY)
+        response.raise_for_status()
+        return response.json().get("conversion_rates", {})
+    except requests.RequestException as e:
+        print(f"Fel vid hämtning av växelkurser: {e}")
+        return {}
+
+def get_crypto(symbol:str):
+    parameters = {"symbol": symbol, "convert": "USD"}
+    headers = {"Accepts": "application/json", "X-CMC_PRO_API_KEY": API_KEY}
+    
+    session = Session()
+    session.headers.update(headers)
+    
+    try:
+        response = session.get(API_URL, params=parameters)
+        response.raise_for_status()
+        data = response.json()["data"][symbol]
+        
+        price_usd = data["quote"]["USD"]["price"]
+        
+        return {
+            "name": data["name"],
+            "symbol":data["symbol"],
+            "price_usd": price_usd
+        }
+        
+    except RequestException as e:
+        print(f"Fel vid API-anrop: {e}")
+        return None
+
